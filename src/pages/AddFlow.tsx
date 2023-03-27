5e86f8efcd783807e22109ec9c98dcd33f7b3a16
Rao Zaeem Shahid <raozaeem2018@gmail.com>
2023-03-28 01:04:35 +0500

    Expense Tracking Completed

---
+++ src/pages/AddFlow.tsx
@@ -0,0 +1,84 @@
+import { Dispatch, SetStateAction, useState } from "react";
+
+const AddFlow: React.FC<{
+  addFlow: (props: { name: string; amount: number }) => void;
+  changeIsAdding: Dispatch<SetStateAction<boolean>>;
+}> = ({ addFlow, changeIsAdding }) => {
+  const [name, changeName] = useState("");
+  const [amount, changeAmount] = useState("");
+  const [flowType, changeFlowType] = useState<"Expense" | "Income">("Expense");
+
+  return (
+    <>
+      <div className="m-2 flex flex-col justify-center gap-2">
+        <input
+          type="text"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-1 px-3 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          placeholder="Description"
+          onChange={(e) => changeName(e.target.value)}
+          value={name}
+        />
+        <input
+          type="number"
+          className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-1 px-3 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+          placeholder="Amount"
+          onChange={(e) => changeAmount(e.target.value)}
+          value={amount}
+        />
+        <div className="flex justify-around">
+          <div className="flex gap-1">
+            <input
+              type="checkbox"
+              checked={flowType == "Expense"}
+              onChange={(e) => {
+                if (e.target.checked) changeFlowType("Expense");
+              }}
+            />
+            <h2
+              onClick={() => changeFlowType("Expense")}
+              className="cursor-pointer"
+            >
+              Expense
+            </h2>
+          </div>
+
+          <div className="flex gap-1">
+            <input
+              type="checkbox"
+              checked={flowType == "Income"}
+              onChange={(e) => {
+                if (e.target.checked) changeFlowType("Income");
+              }}
+            />
+            <h2
+              onClick={() => changeFlowType("Income")}
+              className="cursor-pointer"
+            >
+              Income
+            </h2>
+          </div>
+        </div>
+        <button
+          className="w-full py-[2px] rounded-lg   bg-green-600 hover:bg-green-700"
+          onClick={() => {
+            let money: number = 0;
+            if (!amount) money = 0;
+            if (flowType == "Income") money = parseInt(amount);
+            if (flowType == "Expense") money = -1 * parseInt(amount);
+
+            addFlow({
+              amount: money,
+              name,
+            });
+            changeName("");
+            changeAmount("");
+            changeIsAdding(false);
+          }}
+        >
+          Add Flow
+        </button>
+      </div>
+    </>
+  );
+};
+export default AddFlow;
---
+++ src/pages/List.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const List: React.FC<{ list: { name: string; amount: number }[] }> = ({
+  list,
+}) => {
+  const [searchQuery, changeSearchQuery] = useState("");
+  return (
+    <>
+      <div>
+        <div className="my-3">
+          <input
+            type="text"
+            className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-1 px-3 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+            placeholder="Search"
+            onChange={(e) => changeSearchQuery(e.target.value)}
+            value={searchQuery}
+          />
+        </div>
+        {list
+          .filter((item) => item.name.toLowerCase().includes(searchQuery.toLowerCase()))
+          .map((flow) => (
+            <div
+              key={`${Math.random()}`}
+              className={`m-1 border-r-4 bg-gray-700 shadow-md shadow-gray-900 ${
+                flow.amount < 0 ? "border-red-700" : "border-green-700"
+              } flex`}
+            >
+              <div className="mx-2 flex w-full justify-between p-1">
+                <h2>{flow.name}</h2>
+                <h2>${Math.abs(flow.amount)}</h2>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
+export default List;
